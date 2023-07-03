c3ff01f593ad560b5efaf3d2bb877a8702579ed2
Amine <mohamedaminesaidani7@gmail.com>
2023-07-03 17:10:43 +0100

    adding the pagination to the latest created tickets and some fixes

--- src/main/java/com/example/stacktradeapp/config/CorsConfig.java
+++ src/main/java/com/example/stacktradeapp/config/CorsConfig.java
@@ -7,10 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @ComponentScan(basePackages = {"com.example.stacktradeapp.mongodb",
-        "com.example.stacktradeapp.elasticsearch"
-        ,"com.example.stacktradeapp.controllers"
-        ,"com.example.stacktradeapp.entities",
-        "com.example.stacktradeapp.config"})
+        "com.example.stacktradeapp.elasticsearch"})
 public class CorsConfig implements WebMvcConfigurer {
     @Value("${cors.allowedOrigins}")
     private String[] allowedOrigins;
